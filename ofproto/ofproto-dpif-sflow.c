d1b627d976598650a15497d754afccc11a6b144e
Richard Habeeb <richard.habeeb@gmail.com>
2016-07-07 14:40:28 -0500

    Updating checksum for new schema. Fixing sampler/receiver misuse bug.

--- ofproto/ofproto-dpif-sflow.c
+++ ofproto/ofproto-dpif-sflow.c
@@ -777,6 +777,7 @@ dpif_sflow_set_options(struct dpif_sflow *ds,
     receiver = sfl_agent_addReceiver(ds->sflow_agent);
     sfl_receiver_set_sFlowRcvrOwner(receiver, "Open vSwitch sFlow");
     sfl_receiver_set_sFlowRcvrTimeout(receiver, 0xffffffff);
+    sfl_receiver_set_sFlowRcvrMaximumDatagramSize(receiver, ds->options->rcvr_max_len);
 
     /* Set the sampling_rate down in the datapath. */
     ds->probability = MAX(1, UINT32_MAX / ds->options->sampling_rate);
@@ -790,7 +791,6 @@ dpif_sflow_set_options(struct dpif_sflow *ds,
     sampler = sfl_agent_addSampler(ds->sflow_agent, &dsi);
     sfl_sampler_set_sFlowFsPacketSamplingRate(sampler, ds->options->sampling_rate);
     sfl_sampler_set_sFlowFsMaximumHeaderSize(sampler, ds->options->header_len);
-    sfl_receiver_set_sFlowRcvrMaximumDatagramSize(sampler, ds->options->rcvr_max_len);
     sfl_sampler_set_sFlowFsReceiver(sampler, RECEIVER_INDEX);
 
     /* Add a counter poller for the bridge so we can use it to send
